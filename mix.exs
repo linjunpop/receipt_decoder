666a2daeec4a53c2232bdb028fd0eeac9fb729f4
Jun Lin <linjunpop@gmail.com>
2017-08-10 09:19:49 +0800

    Update project desc.

--- mix.exs
+++ mix.exs
@@ -1,12 +1,17 @@
 defmodule ReceiptDecoder.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :receipt_decoder,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
       docs: [
         main: "ReceiptDecoder"
@@ -25,6 +30,24 @@ defmodule ReceiptDecoder.Mixfile do
   defp deps do
     [
       {:ex_doc, "~> 0.15", only: [:dev, :docs]}
+    ]
+  end
+
+  defp description do
+    "Decode iOS App receipt"
+  end
+
+  defp package do
+    [
+      name: :receipt_decoder,
+      files: ["lib/receipt_decoder*", "src/ReceiptModule.{erl,hrl}", "mix.exs", "README*", "LICENSE"],
+      maintainers: ["Jun Lin"],
+      licenses: ["MIT"],
+      source_url: "https://github.com/linjunpop/receipt_decoder",
+      homepage_url: "https://github.com/linjunpop/receipt_decoder",
+      links: %{
+        "GitHub" => "https://github.com/linjunpop/receipt_decoder"
+      }
     ]
   end
 end
