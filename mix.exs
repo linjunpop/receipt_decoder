b65c64b8b8d167fac03fa452921cbf3c874acb80
Jun Lin <linjunpop@gmail.com>
2017-08-10 08:42:03 +0800

    Added travis-ci config.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ReceiptDecoder.Mixfile do
     [
       app: :receipt_decoder,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.3",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
